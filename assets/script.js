a415e211bf7dfe0ced0faf6ad766a280ffddd451
OnceYT <80458521+OnceYT@users.noreply.github.com>
2023-02-14 15:06:17 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,49 @@
 VANTA.FOG({
-el: "body",
+  el: ".bg",
   mouseControls: true,
   touchControls: true,
   gyroControls: false,
   minHeight: 200.00,
   minWidth: 200.00,
-  highlightColor: 0x8b8b8b,
-  midtoneColor: 0x5d5d5d,
-  lowlightColor: 0x272727,
-  baseColor: 0x0
-})
+  highlightColor: 0x1a1e69,
+  midtoneColor: 0xf385c,
+  lowlightColor: 0x146b6b,
+  baseColor: 0x4fbccd,
+  blurFactor: 0.3,
+  speed: 0.5,
+  zoom: 0.45
+});
+
+const toggleButton = document.querySelector("#sound-toggle");
+let isSoundOn = false;
+const audioElement = new Audio("assets/media/playback.mp3");
+
+toggleButton.addEventListener("click", function() {
+  if (isSoundOn) {
+    toggleButton.innerHTML = '<i class="uil uil-volume-mute"></i>';
+    audioElement.pause();
+  } else {
+    toggleButton.innerHTML = '<i class="uil uil-volume-up"></i>';
+    audioElement.play();
+  }
+  isSoundOn = !isSoundOn;
+});
 
 var typed = new Typed('#type', {
-  strings: ["fabulous", "lovely", "serene", "mesmerizing", "terrific", "great", "delightful", "magical", "wonderful", "awesome", "splendid", "gorgeous", "rewarding", "glorious", "royal", "beautiful"],
+  strings: ["a fabulous", "a lovely", "a serene", "a mesmerizing", "a terrific", "a great", "a delightful", "a magical", "an wonderful", "an awesome", "a splendid", "a gorgeous", "a rewarding", "a glorious", "a royal", "a beautiful", "a tranquil", "an enchanted", "an uplifting", "an ecstatic"],
   typeSpeed: 150,
   backSpeed: 50,
-  backDelay: 2250,
+  backDelay: 2260,
   loop: true,
+  shuffle: true
 });
 
 window.onload = function(){
   var dayNumber = new Date().getDay();
   const Days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   document.getElementById('day').innerHTML = Days[dayNumber]
+  const Greetings = ["Hello", "Hola", "Konnichiwa", "Bonjour", "Ni hao", "Ciao", "Salam", "Hallo", "Hoi", "Salut", "Ola", "Namaste"];
+  var randomGreeting = Greetings[Math.floor(Math.random() * Greetings.length)];
+  document.getElementById('hello').innerHTML = randomGreeting
 };
 
