c89eab80e8816abd1c06a8b847cf4a7cde2783bf
OnceYT <80458521+OnceYT@users.noreply.github.com>
2023-04-03 11:12:52 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,101 @@
-VANTA.FOG({
-  el: ".bg",
-  highlightColor: 0x1a1e69,
-  midtoneColor: 0xf385c,
-  lowlightColor: 0x146b6b,
-  baseColor: 0x4fbccd,
-  blurFactor: 0.3,
-  speed: 0.5,
-  zoom: 0.4
+const toggleColorButton = document.querySelector("#color-mode-toggle");
+let isLightMode = false;
+
+function isDarkModeEnabled() {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        toggleColorButton.innerHTML = '<i class="uil uil-moon"></i>';
+        isLightMode = true;
+        setTimeout(function () {
+            vanta = VANTA.FOG({
+                el: "#bg",
+                highlightColor: 0x313130,
+                midtoneColor: 0xbababa,
+                lowlightColor: 0x3c3c3e,
+                baseColor: 0x0,
+                blurFactor: 0.3,
+                speed: 0.5,
+                zoom: 0.4
+            });
+        }, 6200);
+    } else {
+        toggleColorButton.innerHTML = '<i class="uil uil-sun"></i>';
+        isLightMode = false;
+        setTimeout(function () {
+            vanta = VANTA.FOG({
+                el: "#bg",
+                highlightColor: 0x1a1e69,
+                midtoneColor: 0xf385c,
+                lowlightColor: 0x146b6b,
+                baseColor: 0x4fbccd,
+                blurFactor: 0.3,
+                speed: 0.5,
+                zoom: 0.4
+            });
+        }, 6200);
+    }
+}
+
+const background = document.querySelector("#bg");
+toggleColorButton.addEventListener("click", function() {
+  toggleColorButton.style.opacity = 0.83;
+  background.style.opacity = 1;
+  toggleColorButton.style.top = '10px';
+  toggleColorButton.classList.remove("color-mode-toggle-reveal")
+  background.classList.remove("background-reveal")
+  background.classList.add("background-animate");
+  toggleColorButton.classList.add("color-mode-toggle-click-animate");
+    setTimeout(function() {
+      background.style.opacity = 1;
+      toggleColorButton.style.opacity = 0.83;
+      background.classList.remove("background-animate");
+      toggleColorButton.classList.remove("color-mode-toggle-click-animate");
+    }, 1000);
+  if (isLightMode) {
+    lightOptions = {
+      highlightColor: 0x1a1e69,
+      midtoneColor: 0xf385c,
+      lowlightColor: 0x146b6b,
+      baseColor: 0x4fbccd
+    }
+    setTimeout(function() {
+      vanta.setOptions(lightOptions);
+      toggleColorButton.innerHTML = '<i class="uil uil-sun"></i>';
+    }, 500);
+  } else {
+    darkOptions = {
+      highlightColor: 0x313130,
+      midtoneColor: 0xbababa,
+      lowlightColor: 0x3c3c3e,
+      baseColor: 0x0
+    }
+    setTimeout(function () {
+      vanta.setOptions(darkOptions);
+      toggleColorButton.innerHTML = '<i class="uil uil-moon"></i>';
+    }, 500);
+  }
+  isLightMode = !isLightMode;
 });
 
-const toggleButton = document.querySelector("#sound-toggle");
+const toggleSoundButton = document.querySelector("#sound-toggle");
 let isSoundOn = false;
 const audioElement = new Audio("assets/media/playback.mp3");
-toggleButton.addEventListener("click", function() {
+toggleSoundButton.addEventListener("click", function() {
+  toggleSoundButton.style.opacity = 0.83;
+  toggleSoundButton.style.left = '10px';
+  toggleSoundButton.classList.remove('sound-toggle-reveal');
+  toggleSoundButton.classList.add('sound-toggle-click-animate');
+  setTimeout(function() {
+    toggleSoundButton.classList.remove('sound-toggle-click-animate');
+  }, 1000);
   if (isSoundOn) {
-    toggleButton.innerHTML = '<i class="uil uil-volume-mute"></i>';
+    setTimeout(function() {
+    toggleSoundButton.innerHTML = '<i class="uil uil-volume-mute"></i>';
+    }, 500);
     audioElement.pause();
   } else {
-    toggleButton.innerHTML = '<i class="uil uil-volume-up"></i>';
+    setTimeout(function() {
+      toggleSoundButton.innerHTML = '<i class="uil uil-volume-up"></i>';
+    }, 500);
     audioElement.play();
   }
   isSoundOn = !isSoundOn;
@@ -40,3 +118,5 @@ window.onload = function(){
   var randomGreeting = Greetings[Math.floor(Math.random() * Greetings.length)];
   document.getElementById('hello').innerHTML = randomGreeting
 };
+
+isDarkModeEnabled()
